9e57516766b3d67efd9cf42f465eae3b5c4f263b
wissem gharbi <55883112+Xkiller92@users.noreply.github.com>
2023-07-07 10:42:57 +0100

    zezeze

--- powershell_tracker_script/combination.ps1
+++ powershell_tracker_script/combination.ps1
@@ -1,3 +1,10 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$server_url
+)
+
+echo "hellp $server_url"
+
 $capacity = foreach ($disk in Get-WmiObject -Class Win32_DiskDrive)
 {
     [Math]::Round(($disk.Size / 1GB), 2)
@@ -8,8 +15,25 @@ $total_space = "$(($capacity | Measure-Object -Sum).Sum)GB"
 
 $FreeSpace = [Math]::Round(((Get-CimInstance Win32_Volume -Filter "DriveLetter = 'C:'").FreeSpace / 1GB),2)
 
-echo $FreeSpace
-
 $ramCapacity = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | Select-Object -ExpandProperty Sum
 $ramCapacityGB = [Math]::Round(($ramCapacity / 1GB), 2)
-echo $ramCapacityGB  
+
+$url = "hiding_this_for_obvious_reasons" # Request URL
+
+$nummer = Read-Host("Skriv inn mobil nummer: ")
+$tekst = Read-Host("Skriv inn melding: ")
+
+#Request Payload information
+$JSON = @'
+{
+            "content": "$tekst",
+            "senderNumber": "SSB",
+            "targetNumbersAsDelimitedString": "47$nummer",
+            "sendDate": null,
+            "status": {"id": "5"},
+            "numberOfTargetNumbers": null,
+            "numberOfInvalidNumbers": ""
+}
+'@
+
+Invoke-WebRequest -Uri $url -Method Post -Body $JSON -ContentType "application/json"
--- powershell_tracker_script/startup.ps1
+++ powershell_tracker_script/startup.ps1
@@ -1,5 +1,15 @@
 #get input from user
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$server_url,
 
-foreach ($name in Get-Content $filePath) {
-    Invoke-Command -ComputerName $name -FilePath "combination.ps1"
+    [Parameter(Mandatory=$true)]
+    [string]$workstation_list_path,
+
+    [Parameter(Mandatory=$true)]
+    [string]$server_url,
+)
+
+foreach ($name in Get-Content $workstation_list_path) {
+    Invoke-Command -ComputerName $name -FilePath "combination.ps1 -server_url $server_url"
 }
